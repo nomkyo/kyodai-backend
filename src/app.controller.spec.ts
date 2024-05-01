29839b917050a6f49f3724be3545ac9b84df6647
dan <dknomura@gmail.com>
2024-05-01 15:22:53 -0700

    fix tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -17,11 +17,6 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
-    });
-  });
   describe('hello/:name', () => {
     it('should return "Hello ${name}!"', () => {
       const name = chance.name();
